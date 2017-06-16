009028bec765c973f4b3b5dd179cb0bf4b21c579
nt <381836999@qq.com>
2017-06-16 17:17:28 +0800

    change drag.html and drag.js

--- drag.js
+++ drag.js
@@ -1,4 +1,7 @@
 window.onload=function(){
-	document.querySelector("#box").style.color="aqua";
-	console.log('我是缓存区的样子')
+	var box = document.querySelector("#box").style.color="aqua";
+	console.log('我是缓存区的样子');
+	box.onclick=function(){
+		this.innerHTML = '哈哈';
+	}
 }
